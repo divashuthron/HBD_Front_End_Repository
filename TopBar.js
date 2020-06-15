430f89aa426fc49c3131621a22d6d238ace44763
RH <divashuthron@gmail.com>
2020-06-15 23:23:02 +0900

    20200615 Top내비게이션 수정

--- TopBar.js
+++ TopBar.js
@@ -1,26 +1,26 @@
-var dateTarget = document.getElementById("getDate");
-var dOWTarget = document.getElementById("getDOW");
-var clockTarget = document.getElementById("getClock");
+var dateTarget = document.getElementById("text_getDate");
+var dOWTarget = document.getElementById("text_getDow");
+var clockTarget = document.getElementById("text_getClock");
+
+var date=new Date();
 
 function getDate() {
-  var date=new Date();
   var month=date.getMonth();
   var clockDate=date.getDate();
   dateTarget.innerText=`${month+1}월 ${clockDate}일`;
 }
 
 function getDOW() {
-  var date = new Date();
   var day = date.getDay();
   var week = ['일', '월', '화', '수', '목', '금', '토'];
   dOWTarget.innerText = `${week[day]}`;
 }
 
 function getClock() {
-  var date = new Date();
-  var hours=date.getHours();
-  var minutes=date.getMinutes();
-  var seconds=date.getSeconds();
+  var clockdt=new Date();
+  var hours=clockdt.getHours();
+  var minutes=clockdt.getMinutes();
+  var seconds=clockdt.getSeconds();
   clockTarget.innerText = `요일 ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes }`  : minutes }:${seconds < 10 ? `0${seconds }`  : seconds }`;
 }
 
@@ -28,8 +28,6 @@ function init() {
   getDate();
   getDOW();
   getClock();
-  setInterval(getDate, 1000);
-  setInterval(getDOW, 1000);
   setInterval(getClock, 1000);
 }
 
