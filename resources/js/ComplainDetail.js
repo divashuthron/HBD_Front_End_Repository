eb9aec0d3e4f9a1376663a8526fdc0d802b6166e
Eunseong0103 <eunseong0103@naver.com>
2020-08-04 15:28:52 +0900

    20200804

--- resources/js/ComplainDetail.js
+++ resources/js/ComplainDetail.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-    $('select').material_select();
-    
-    $('div', '#top_content').hover(function() {
-        $('h2', '#top_title').css("font-size", "56px");
-        $('h2', '#top_title').css("cursor", "pointer");
-        $('h2', '#top_title').css("transition", "all 0.2s");
-    }, function() {
-        $('h2', '#top_title').css("font-size", "48px");
-        $('h2', '#top_title').css("cursor", "normal");
-        $('h2', '#top_title').css("transition", "all 0.2s");
+    $('#wrapper_top').hover(function() {
+        $('h1').css("transform", "scale(1.2)");
+        $('h1').css("cursor", "pointer");
+        $('h1').css("transition", "all 0.2s");
+    }, function(){
+        $('h1').css("transform", "scale(1.0)");
+        $('h1').css("cursor", "normal");
+        $('h1').css("transition", "all 0.2s");
     });
 });
